d04c386cab6ffb9452bd8a14cd939ce87dfd4b01
xadii8944 <xadii8944@gmail.com>
2024-02-01 18:46:12 +0100

    funkcja get filename

--- index.py
+++ index.py
@@ -6,3 +6,9 @@ import os
 import uuid
 import json
 from flask import send_from_directory
+
+def get_filename_from_json(file_code):
+    with open("filename.json", 'r') as file:
+        json_data = json.load(file)
+    if file_code in json_data:
+        return json_data[file_code]
