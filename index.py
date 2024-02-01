71009992f7c3413d51fc8aa9b7147e3267cdebad
Michał Moskal <michalmoskal02233@gmail.com>
2024-02-01 19:02:10 +0100

    @app.route

--- index.py
+++ index.py
@@ -28,3 +28,31 @@ def get_filename_from_json(file_code):
         json_data = json.load(file)
     if file_code in json_data:
         return json_data[file_code]
+    
+@app.route('/')
+def send_report():
+    return send_from_directory('./public', 'index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    filename = file.filename
+    encrypted_filename = encrypt_file(file, filename)
+    return f'File "{filename}" uploaded and encrypted. File Code:{encrypted_filename}.'
+
+@app.route('/download/<file_code>', methods=['GET'])
+def download(file_code):
+    decrypted_filename = decrypt_file(file_code)
+    path_decrypted_file = os.path.join(decrypted_folder, decrypted_filename)
+    return send_file(path_decrypted_file, as_attachment=True)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+
+if __name == '__main':
+    key = generate_key()
+    app.run()
